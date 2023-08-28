f04996dc3fb60ba4381f30114f405fe3afc01c86
adriangutierrezd <adriangutierrezdelaosa@gmail.com>
2023-08-28 20:18:48 +0200

    refactor: :recycle: Adding constant files
    Adding constans file with HTTP_RESPONSE_CODES

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+require_once __DIR__.'/../config/constants.php';
+
 class BaseController{
 
 
--- app/controllers/TokenController.php
+++ app/controllers/TokenController.php
@@ -21,9 +21,9 @@ class TokenController extends BaseController{
         try{
             $request['iat'] = time();
             $jwt = JWT::encode($request, $_ENV['JWT_KEY'], $_ENV['JWT_ALGORITHM']);
-            $this->httpResponse(200, 'Token generated', ['token' => $jwt, 'expires' => $_ENV['JWT_TTL']]);
+            $this->httpResponse(HTTP_CODE_OK, 'Token generated', ['token' => $jwt, 'expires' => $_ENV['JWT_TTL']]);
         }catch(\Exception $e){
-            $this->httpResponse(500, 'Error generating token', ['error' => $e->getMessage()]);
+            $this->httpResponse(HTTP_CODE_INTERNAL_SERVER_ERROR, 'Error generating token', ['error' => $e->getMessage()]);
         }
     }
 
