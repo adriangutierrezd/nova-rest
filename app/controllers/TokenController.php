be640113b6f8a89151b0c73c1689a754cc03b3cc
adriangutierrezd <adriangutierrezdelaosa@gmail.com>
2023-08-28 20:23:54 +0200

    feat(Tokens): :goal_net: Handeling JWT error on generateToken function

--- app/controllers/TokenController.php
+++ app/controllers/TokenController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+use UnexpectedValueException;
 use Dotenv;
 use Exception;
 
@@ -22,6 +23,8 @@ class TokenController extends BaseController{
             $request['iat'] = time();
             $jwt = JWT::encode($request, $_ENV['JWT_KEY'], $_ENV['JWT_ALGORITHM']);
             $this->httpResponse(HTTP_CODE_OK, 'Token generated', ['token' => $jwt, 'expires' => $_ENV['JWT_TTL']]);
+        }catch (UnexpectedValueException $e) {
+            $this->httpResponse(HTTP_CODE_INTERNAL_SERVER_ERROR, 'Error generating token', ['error' => $e->getMessage()]);
         }catch(\Exception $e){
             $this->httpResponse(HTTP_CODE_INTERNAL_SERVER_ERROR, 'Error generating token', ['error' => $e->getMessage()]);
         }
